ea1e605623f82f6e8e1360e7156bd90f5e242c91
mrrezaahmadi <mrrezaahmadi>
2020-08-02 23:19:12 +0430

    Updates

---
+++ precache-manifest.5c4b54450045ad018de15d0bd4a0d248.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810014c9b23381760cf280b4a4e5bcf7",
+    "url": "/ReactJS-Soally_CRM/index.html"
+  },
+  {
+    "revision": "a43f8aecb60e70081448",
+    "url": "/ReactJS-Soally_CRM/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fca04e79b585aa9d82b",
+    "url": "/ReactJS-Soally_CRM/static/css/main.1ddcda11.chunk.css"
+  },
+  {
+    "revision": "a43f8aecb60e70081448",
+    "url": "/ReactJS-Soally_CRM/static/js/2.b8494bca.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ReactJS-Soally_CRM/static/js/2.b8494bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fca04e79b585aa9d82b",
+    "url": "/ReactJS-Soally_CRM/static/js/main.95337468.chunk.js"
+  },
+  {
+    "revision": "8340bd14efba1bca34e3",
+    "url": "/ReactJS-Soally_CRM/static/js/runtime-main.c95ce86a.js"
+  },
+  {
+    "revision": "feff963fc8b3ade0dc1d8413a44a476b",
+    "url": "/ReactJS-Soally_CRM/static/media/SignPainterHouseScriptRegular.feff963f.ttf"
+  },
+  {
+    "revision": "41e49eab42867016423011dccc96c22a",
+    "url": "/ReactJS-Soally_CRM/static/media/YekanBakh-Bold.41e49eab.ttf"
+  },
+  {
+    "revision": "7296ae79f6faca68798b00fbbbee4c22",
+    "url": "/ReactJS-Soally_CRM/static/media/YekanBakh-Regular.7296ae79.ttf"
+  }
+]);
