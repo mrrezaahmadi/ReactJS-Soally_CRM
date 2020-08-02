31606a2fc4c2463db1e8d27d9c25184b7c53406b
mrrezaahmadi <mrrezaahmadi>
2020-08-02 17:25:08 +0430

    add start page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from 'reactstrap'
+import './App.scss'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="start-page">
+        <section className="main">
+          <div className="start-page--title">فرم نظرسنجی و ارتقا سطح خدمت رسانی </div>
+          <div className="logo">logo placeholder</div>
+          <button className="start-page--button">شروع</button>
+        </section>
+        <footer className="footer">
+          <div className="footer-header">
+            <div className="footer-header--title">قدرت گرفته از</div>
+            <button className='footer-header--title--logo'>Soally</button>
+          </div>
+          <div className="footer--subtitle">پلتفرم طراحی فرم‌های CRM</div>
+        </footer>
+      </div>
+    </Container>
   );
 }
 
